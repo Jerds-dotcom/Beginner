24345b8a2601a26c755be341b5fd187e601736bf
Jerds-dotcom <80202787+Jerds-dotcom@users.noreply.github.com>
2021-03-17 02:16:49 -0600

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,7 @@
-from functools import lru_cache
-
-@lru_cache(maxsize=128)
-def fib(n):
-	if n < 2:
+def fib(n, memoization = {}):
+	if n in memoization:
+		return memoization[n]
+	if n <= 2:
 		return n
-	return fib(n-1) + fib(n-2)
-
-if __name__ == "__main__":
-	print(fib(50))
+	memoization[n] = fib(n-1, memoization) + fib(n-2, memoization)
+	return memoization[n]
